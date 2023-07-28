6dacb1596bd7e58fe23235dbf5576b43039fe2ac
idabi8 <i.l.t.l.i@hotmail.it>
2023-07-28 12:16:32 +0200

    Passing Data via Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import ListGroup1 from "./components/ListGroup1";
 import ListGroup2 from "./components/ListGroup2";
+import ListGroup3 from "./components/ListGroup3";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Rome"];
+
   return (
     <div>
-      <ListGroup2 />
+      <ListGroup3 items={items} heading="Cities" />
     </div>
   );
 }
--- src/components/ListGroup3.tsx
+++ src/components/ListGroup3.tsx
@@ -1,1 +1,41 @@
-// Reusable ListGroup
+import React, { useState } from "react";
+
+/* Passing Data via Props (propreties), Reusable component */
+
+/**Props are the inputs to our components. First we need to decide the shape of the input to this component: in our case we want to pass on object with 2 properties: 
+{ itemes: [], heading: string }. To that we use the TS interface: */
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+/**Render a list of items dynamically */
+function ListGroup3({ items, heading }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  /* Returns a stateful value (selectedIndex), and a function to update it.*/
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup3;
