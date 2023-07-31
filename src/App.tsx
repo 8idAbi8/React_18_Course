82a90d7989646f773cef66359cae0501a45e5b06
idabi8 <i.l.t.l.i@hotmail.it>
2023-07-31 10:17:38 +0200

    added MyButtonComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import Alert from "./components/Alert";
+import MyButtonComponent from "./components/MyButtonComponent ";
 
 function App() {
   return (
-    <div>
-      {/**Alert component accepts a single prop called children, which represents the content enclosed in <Alert>...</Alert> tags */}
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
-    </div>
+    <button>
+      <MyButtonComponent />
+    </button>
   );
 }
 
---
+++ src/components/MyButtonComponent .tsx
@@ -0,0 +1,9 @@
+const MyButtonComponent = () => {
+  return (
+    <button type="button" className="btn btn-success">
+      Success
+    </button>
+  );
+};
+
+export default MyButtonComponent;
