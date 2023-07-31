d6d32e33abdd0c496f2a40c767c77bd0732a696a
idabi8 <i.l.t.l.i@hotmail.it>
2023-07-31 11:28:04 +0200

    added button with dynamic text

--- src/components/MyButtonComponent .tsx
+++ src/components/MyButtonComponent .tsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 const MyButtonComponent = () => {
+  // state hook
+  const [buttonText, setbuttonText] = useState("2+2=");
+
+  // handle event function
+  const handleClick = () => {
+    if (buttonText === "2+2=") setbuttonText("3+1=");
+    else setbuttonText("2+2=");
+  };
+
   return (
-    <button type="button" className="btn btn-success">
-      Success
-    </button>
+    <>
+      <button type="button" className="btn btn-success" onClick={handleClick}>
+        {buttonText}
+      </button>
+    </>
   );
 };
 
